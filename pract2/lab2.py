c684c5ed1e92ab3aa443179f016ce40aecbfa3ae
MarianCabrera <marian.cabrera@e-campus.uab.cat>
2021-03-15 17:26:04 +0100

    center images done

--- pract2/lab2.py
+++ pract2/lab2.py
@@ -13,15 +13,27 @@ def getCenteredImages(reference, image):
     center = np.unravel_index(np.argmax(conv), conv.shape)
     
     diff = [centerRef[0] - center[0], centerRef[1] - center[1]]
-    print(reference.shape)
     print(diff)
     
-    new = np.zeros((reference.shape[0], reference.shape[0]), dtype=float)
-    if diff[0]>=0:
-        if diff[1]>=0:
-            new[diff[0]:, diff[1]:] = images[0:940-diff[0],0:940-diff[1],1]
-
-
+    new = np.roll(image,diff[0],axis=0)
+    new = np.roll(new,diff[1],axis=1)
+    
+    # if diff[0]>=0:
+    #     if diff[1]>=0:
+    #         new[diff[0]:, diff[1]:] = images[0:reference.shape[0]-diff[0],0:reference.shape[1]-diff[1],1]
+    #     else:
+    #         print(reference.shape)
+    #         print(diff)
+    #         #new[diff[0]:, 0:reference.shape[1]+diff[1]] = images[0:940-diff[0],abs(diff[1]):,1]
+    #         new[0:reference.shape[1]+diff[1], diff[0]:] = images[abs(diff[1]):,0:reference.shape[0]-diff[0],1]
+            
+    # else:
+    #     if diff[1]>=0:
+    #         print(reference.shape)
+    #         print(diff)
+    #     else:
+    #         print(reference.shape)
+    #         print(diff)
 
 
     return new
@@ -47,18 +59,20 @@ images[:,:,2] = imgTopRight
 images[:,:,3] = imgBotLeft
 images[:,:,4] = imgBotRight
 
-newImages = np.zeros((imgCenter.shape[0], imgCenter.shape[0], 3 ), dtype=float)
-newImages[:,:,1] = images[:,:,0]
+newImages = np.zeros((imgCenter.shape[0], imgCenter.shape[0], 5 ), dtype=float)
+newImages[:,:,0] = images[:,:,0]
 newImages[:,:,1] = getCenteredImages(images[:,:,0], images[:,:,1])
 newImages[:,:,2] = getCenteredImages(images[:,:,0], images[:,:,2])
+newImages[:,:,3] = getCenteredImages(images[:,:,0], images[:,:,3])
+newImages[:,:,4] = getCenteredImages(images[:,:,0], images[:,:,4])
 
 plt.figure(1)
 plt.imshow(images[:,:,0],'gray')
 plt.figure(2)
-plt.imshow(images[:,:,1],'gray')
+plt.imshow(newImages[:,:,1],'gray')
 plt.figure(3)
-plt.imshow(images[:,:,2],'gray')
+plt.imshow(newImages[:,:,2],'gray')
 plt.figure(4)
-plt.imshow(images[:,:,3],'gray')
+plt.imshow(newImages[:,:,3],'gray')
 plt.figure(5)
-plt.imshow(images[:,:,4],'gray')
+plt.imshow(newImages[:,:,4],'gray')
