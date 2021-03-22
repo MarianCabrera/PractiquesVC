127ae8980b2189b157355e57287b4821464855f9
MarianCabrera <marian.cabrera@e-campus.uab.cat>
2021-03-22 15:28:13 +0100

    DONE

--- pract2/lab2_araSi.py
+++ pract2/lab2_araSi.py
@@ -1,4 +1,5 @@
 import os
+import time
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -21,6 +22,7 @@ def getFFTconv(reference, image):
     conv = signal.fftconvolve(referenceMean, imageMean[::-1,::-1], mode='same')
     center = np.unravel_index(np.argmax(conv), conv.shape)
     diff = [center[0] - centerRef[0], center[1] - centerRef[1]]
+    
     new = np.roll(image,diff[0],axis=0)
     new = np.roll(new,diff[1],axis=1)
     return new
@@ -37,25 +39,7 @@ def getCrossCorr(reference, image):
     
     new = np.roll(image,x-xRef,axis=1)
     new = np.roll(new,y-yRef,axis=0)
-    
-    # fig, (ax_orig, ax_template, ax_corr, ax_result) = plt.subplots(4, 1, figsize=(6, 15))
-    # ax_orig.imshow(reference, cmap='gray')
-    # ax_orig.set_title('Original')
-    # ax_orig.set_axis_off()
-    # ax_template.imshow(image, cmap='gray')
-    # ax_template.set_title('Template') 
-    # ax_template.set_axis_off() 
-    # ax_corr.imshow(corr, cmap='gray') 
-    # ax_corr.set_title('Cross-correlation') 
-    # ax_corr.set_axis_off() 
-    # ax_orig.plot(x, y, 'ro')
-    # ax_orig.plot(xRef, yRef, 'ro') 
-    # ax_result.imshow(new, cmap='gray')
-    # ax_result.set_title('result') 
-    # ax_result.set_axis_off() 
-    
-    # fig.show()
-    
+
     return new
 
 def getCorr(reference, image):
@@ -73,9 +57,10 @@ def getCorr(reference, image):
     
     return new
 
-def alignImages(ref, im2, im3, alignType):
+def alignImages(ref, im2, im3, alignType, name):
     shape = ref.shape
     result = np.zeros((shape[0],shape[1],3))
+    t=time.time()
     if alignType == 0:
         result[:,:,0] = ref
         result[:,:,1] = getFFTconv(ref, im2)
@@ -88,42 +73,63 @@ def alignImages(ref, im2, im3, alignType):
         result[:,:,0] = ref
         result[:,:,1] = getCorr(ref, im2)
         result[:,:,2] = getCorr(ref, im3)
-        
+    elapsed=time.time()-t
+    print(name + ': Elapsed time is '+str(elapsed)+' seconds')
     
     return result
 
-def saveImg(img, name):
-    n = os.path.splitext(name)[0] + "_color.png"
+def saveImg(img, name, extension):
+    n = os.path.splitext(name)[0] + "_color" + extension
     cv2.imwrite(n, img)
     return 0
 
 # ----------------------------------------------------------------------------
 
 
-name = 'img/test1.jpg'
-test1 = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+path = "img/"
+imageSet = "p"
+numberImgs = 4
+extension = ".jpg"
+alignType = 0
 
+for i in range(1,numberImgs+1):
+    name = path + imageSet + str(i) + extension
+    img =  cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+    r, g, b = cut(img)
+    result = alignImages(r,g,b,alignType, name)
+    saveImg(result, path + imageSet + str(i), extension)
+    # result.append(alignImages(r,g,b,alignType))
+    
 # plt.figure(1)
-# plt.imshow(test1,'gray')
+# plt.imshow(result[0].astype(np.uint8),'gray')
 
-test1_R, test1_G, test1_B = cut(test1)
-test1_R = test1_R.astype(np.double)
-test1_G = test1_G.astype(np.double)
-test1_B = test1_B.astype(np.double)
 
-# plt.figure(2)
-# plt.imshow(test1_R,'gray')
-# plt.figure(3)
-# plt.imshow(test1_G,'gray')
-# plt.figure(4)
-# plt.imshow(test1_B,'gray')
 
-result_test1 = alignImages(test1_R, test1_G, test1_B, 2)
 
-plt.figure(10)
-plt.imshow(result_test1.astype(np.uint8))
 
-saveImg(result_test1, name)
+# name = 'img/test1.jpg'
+# test1 = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+
+
+
+# test1_R, test1_G, test1_B = cut(test1)
+# test1_R = test1_R.astype(np.double)
+# test1_G = test1_G.astype(np.double)
+# test1_B = test1_B.astype(np.double)
+
+# # plt.figure(2)
+# # plt.imshow(test1_R,'gray')
+# # plt.figure(3)
+# # plt.imshow(test1_G,'gray')
+# # plt.figure(4)
+# # plt.imshow(test1_B,'gray')
+
+# result_test1 = alignImages(test1_R, test1_G, test1_B, 0)
+
+# plt.figure(10)
+# plt.imshow(result_test1.astype(np.uint8))
+
+# saveImg(result_test1, name)
 
 
 
