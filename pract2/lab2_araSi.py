f68ef5ecaf49dfb3beb6e41de57dddb87e65534d
MarianCabrera <marian.cabrera@e-campus.uab.cat>
2021-03-22 22:37:49 +0100

    Revert "mode"
    This reverts commit 0e6a93badb7cc854e50f51c2a8fd390f2b4d8319.

--- pract2/lab2_araSi.py
+++ pract2/lab2_araSi.py
@@ -13,18 +13,13 @@ def cut(img):
     img_3_3 = img[size*2:size*3,:]
     return img_3_1, img_3_2, img_3_3
 
-def getFFTconv(reference, image, mode):
+def getFFTconv(reference, image):
     referenceMean = reference.astype(np.double) - reference.mean().astype(np.double) 
     imageMean = image.astype(np.double) - image.mean().astype(np.double)
     
-    if mode == 0:
-        convRef = signal.fftconvolve(referenceMean, referenceMean[::-1,::-1], mode='same')
-        conv = signal.fftconvolve(referenceMean, imageMean[::-1,::-1], mode='same')
-    else:
-        convRef = signal.fftconvolve(referenceMean, referenceMean[::-1,::-1])
-        conv = signal.fftconvolve(referenceMean, imageMean[::-1,::-1])
-        
+    convRef = signal.fftconvolve(referenceMean, referenceMean[::-1,::-1], mode='same')
     centerRef = np.unravel_index(np.argmax(convRef), convRef.shape)
+    conv = signal.fftconvolve(referenceMean, imageMean[::-1,::-1], mode='same')
     center = np.unravel_index(np.argmax(conv), conv.shape)
     diff = [center[0] - centerRef[0], center[1] - centerRef[1]]
     
@@ -36,14 +31,10 @@ def getCrossCorr(reference, image):
     referenceMean = reference.astype(np.double) - reference.mean().astype(np.double) 
     imageMean = image.astype(np.double) - image.mean().astype(np.double)
     
-    if mode == 0:
-        corrRef = signal.correlate2d(referenceMean, referenceMean, boundary='symm', mode='same')
-        corr = signal.correlate2d(referenceMean, imageMean, boundary='symm', mode='same')
-    else:
-        corrRef = signal.correlate2d(referenceMean, referenceMean, boundary='symm')
-        corr = signal.correlate2d(referenceMean, imageMean, boundary='symm')
-        
+    corrRef = signal.correlate2d(referenceMean, referenceMean, boundary='symm', mode='same')
     yRef, xRef = np.unravel_index(np.argmax(corrRef), corrRef.shape)
+    
+    corr = signal.correlate2d(referenceMean, imageMean, boundary='symm', mode='same')
     y, x = np.unravel_index(np.argmax(corr), corr.shape)
     
     new = np.roll(image,x-xRef,axis=1)
@@ -55,14 +46,10 @@ def getCorr(reference, image):
     referenceMean = reference.astype(np.double) - reference.mean().astype(np.double) 
     imageMean = image.astype(np.double) - image.mean().astype(np.double)
     
-    if mode == 0:
-        corrRef = signal.convolve2d(referenceMean, referenceMean[::-1,::-1], boundary='symm', mode='same')
-        corr = signal.convolve2d(referenceMean, imageMean[::-1,::-1], boundary='symm', mode='same')
-    else:
-        corrRef = signal.convolve2d(referenceMean, referenceMean[::-1,::-1], boundary='symm')
-        corr = signal.convolve2d(referenceMean, imageMean[::-1,::-1], boundary='symm')
-        
+    corrRef = signal.convolve2d(referenceMean, referenceMean[::-1,::-1], boundary='symm', mode='same')
     yRef, xRef = np.unravel_index(np.argmax(corrRef), corrRef.shape)
+    
+    corr = signal.convolve2d(referenceMean, imageMean[::-1,::-1], boundary='symm', mode='same')
     y, x = np.unravel_index(np.argmax(corr), corr.shape)
     
     new = np.roll(image,x-xRef,axis=1)
@@ -70,18 +57,18 @@ def getCorr(reference, image):
     
     return new
 
-def alignImages(ref, im2, im3, alignType, name, mode):
+def alignImages(ref, im2, im3, alignType, name):
     shape = ref.shape
     result = np.zeros((shape[0],shape[1],3))
     t=time.time()
     if alignType == 0:
         result[:,:,0] = ref
-        result[:,:,1] = getFFTconv(ref, im2, mode)
-        result[:,:,2] = getFFTconv(ref, im3, mode)
+        result[:,:,1] = getFFTconv(ref, im2)
+        result[:,:,2] = getFFTconv(ref, im3)
     elif alignType == 1:
         result[:,:,0] = ref
-        result[:,:,1] = getCrossCorr(ref, im2, mode)
-        result[:,:,2] = getCrossCorr(ref, im3, mode)
+        result[:,:,1] = getCrossCorr(ref, im2)
+        result[:,:,2] = getCrossCorr(ref, im3)
     elif alignType == 2:
         result[:,:,0] = ref
         result[:,:,1] = getCorr(ref, im2)
@@ -104,7 +91,6 @@ path = "img/set/"
 imageSet = "s"
 extension = ".jpg"
 alignType = 0
-mode = 0
 
 files = os.listdir(path)
 
@@ -112,7 +98,7 @@ for i in range(1,len(files)):
     name = path + imageSet + str(i) + extension
     img =  cv2.imread(name, cv2.IMREAD_GRAYSCALE)
     r, g, b = cut(img)
-    result = alignImages(r,g,b,alignType, name, mode)
+    result = alignImages(r,g,b,alignType, name)
     saveImg(result, path + "results/"+ imageSet + str(i), extension)
     plt.figure(i)
     plt.imshow(result.astype(np.uint8))
